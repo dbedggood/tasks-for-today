266b925ad5ac105cf6b5de46023238ff1eeac18b
Daniel Bedggood <ddbedggood@gmail.com>
2019-09-15 15:27:27 +1200

    Create header, add backgrounds

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="header">
+    <h1 class="brand">Tasks for Today</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'header'
+}
+</script>
+
+<style scoped>
+.header {
+  background-image: url('../assets/tweed.png');
+  background-repeat: repeat;
+  background-position: center;
+  padding: 2em;
+  box-shadow: 0 5px 10px #555;
+}
+
+.brand {
+  text-align: center;
+  margin-bottom: 0;
+  color: aliceblue;
+  font-size: 3rem;
+}
+</style>
