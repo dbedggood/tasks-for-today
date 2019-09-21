076ac726ab02f130937a6b563e2ee27ecf7686e7
Daniel Bedggood <ddbedggood@gmail.com>
2019-09-22 11:36:07 +1200

    Remove vue router history mode
    Tried to remove the /#/ from the url for this SPA
    No server config to help do this with GitHub pages

--- src/main.js
+++ src/main.js
@@ -10,16 +10,9 @@ Vue.config.productionTip = false
 Vue.use(VueRouter)
 Vue.use(VueProgress)
 
-const router = new VueRouter({ mode: 'history', routes })
+const router = new VueRouter({ routes })
 
 new Vue({
   router,
-  render: h => h(App),
-  created() {
-    if (sessionStorage.redirect) {
-      const redirect = sessionStorage.redirect
-      delete sessionStorage.redirect
-      this.$router.push(redirect)
-    }
-  }
+  render: h => h(App)
 }).$mount('#app')
